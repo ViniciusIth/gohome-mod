9bf6b48a8c27a81388c08f4612472d4e45d68a8f
ViniciusIth <vini.ithalo@hotmail.com>
2023-12-19 19:20:07 -0300

    feat: reload config now has some feedback

--- src/main/java/cloud/viniciusith/gohome/command/ReloadConfigCommand.java
+++ src/main/java/cloud/viniciusith/gohome/command/ReloadConfigCommand.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 public class ReloadConfigCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
@@ -14,6 +15,7 @@ public class ReloadConfigCommand {
     }
 
     private static int run(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+        ctx.getSource().sendMessage(Text.of("Reloading Go Home configs"));
         ModConfig.reloadConfigs();
         return 1;
     }
